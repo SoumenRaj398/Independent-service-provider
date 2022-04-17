333251ffa574b18637c5a4d795d749f14767b6bf
SoumenRaj398 <soumenraj66@gmail.com>
2022-04-18 01:47:04 +0600

    clients section

---
+++ src/Pages/Home/Clients/Clients.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import client1 from "../../../images/client1.jpg"
+import client2 from "../../../images/client2.png"
+import client3 from "../../../images/client3.jpg"
+import client4 from "../../../images/client4.jpg"
+import Client from '../Client/Client';
+import "./Clients.css"
+
+const clients =  [
+
+    {id:1,name:"Soumen Raj",img:client1,info:"Thank you for your excellent service. Thank you to Ms. Kai and her team who are so helpful and loyal about their service. They always answered all our questions and concern at all hours without getting annoyed."},
+    {id:2,name:"Marry Christen",img:client2,info:"This is definitely a HIGHLY recommended Agency. A big applause and very grateful to this agency for their efforts and assistance to our visa application. They are truly knowledgeable,skilled and has empathy throughout the process."},
+    {id:3,name:"Ravi Bishno",img:client3,info:"We have been lucky enough to Invite this bands and artists here in this city like Valley of Chrome, Typecast, Gloc-9 and Shantidope through ImmiCo. The events went through smoothly because of this and we greatly appreciate the support ."},
+    {id:4,name:"Anjeli Bhatt",img:client4,info:"I only trust this Consultancy in any advice that they give me because I know they’re giving me the right direction towards reaching my dreams. They are very professional, friendly, helpful.They are truly knowledgeable,skilled and has empathy throughout the process "}
+]
+
+const Clients = () => {
+    return (
+        <div >
+            <h1 className='clients-title'>Our Successful Clients </h1>
+            <div className='clients-container'>
+            {
+                    clients.map(client => <Client
+                       key ={client.id}
+                       client ={client}
+                       
+                       
+                       ></Client>
+                   )
+                }
+            </div>
+              
+
+            
+            
+        </div>
+    );
+};
+
+export default Clients;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Clients from '../Clients/Clients';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Services></Services>
+            <Clients></Clients>
             
         </div>
     );
